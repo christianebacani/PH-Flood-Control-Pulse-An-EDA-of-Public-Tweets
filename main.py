a0b2f57d79013f2a00a2403c5332e8a20c08de7d
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 15:38:34 +0800

    Added new function to execute `get_column_names_and_dtypes`

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ twitter_authors_column_and_row_count = eda.count_rows_and_columns(
 dphwh_flood_control_tweets_column_and_row_count = eda.count_rows_and_columns(
     "data/for_export_dpwh_floodcontrol.csv"
 )
+
+twitter_authors_columns_and_dtypes = eda.get_column_names_and_dtypes(
+    "data/well_known_authors_dpwh_floodcontrol.csv"
+)
