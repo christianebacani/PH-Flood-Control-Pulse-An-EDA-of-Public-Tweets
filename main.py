dc2ba297886a6b5101410b0751d0022395a4d6a0
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 15:15:59 +0800

    Added new module `main.py`

---
+++ main.py
@@ -0,0 +1,10 @@
+from src import eda
+
+eda.extract_files_from_zipfile("data/archive.zip")
+
+twitter_authors_column_and_row_count = eda.count_rows_and_columns(
+    "data/well_known_authors_dpwh_floodcontrol.csv"
+)
+dphwh_flood_control_tweets_column_and_row_count = eda.count_rows_and_columns(
+    "data/for_export_dpwh_floodcontrol.csv"
+)
