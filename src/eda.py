8d25895152c9cf0e4e7e00ee25194441eedf0665
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 15:15:45 +0800

    Added new directory path `src` to store the module to perform EDA

---
+++ src/eda.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from zipfile import ZipFile
+
+def extract_files_from_zipfile(filepath: str) -> None:
+    # Extract all the files inside the zipfile of data/ directory
+    with ZipFile("data/archive.zip", 'r') as zip_file:
+        zip_file.extractall("data") # File destination
+
+def count_rows_and_columns(filepath: str) -> list[int]:
+    df = pd.read_csv(filepath)
+    column_count = len(list(df.keys()))    
+    row_count = len(df)
+
+    return [column_count, row_count]
