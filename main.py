b5d3ab5fb8fd8caff1b78c227c30834764636258
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 16:06:55 +0800

    Rename the existing function from `display_first_and_last_few_rows` to `display_first_few_rows`.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.eda import extract_files_from_zipfile
 from src.eda import count_rows_and_columns
 from src.eda import get_column_names_and_dtypes
+from src.eda import display_first_few_rows
 
 extract_files_from_zipfile("data/archive.zip")
 
@@ -16,4 +17,8 @@ twitter_authors_columns_and_dtypes = get_column_names_and_dtypes(
 )
 dpwh_flood_control_tweets_columns_and_dtypes = get_column_names_and_dtypes(
     "data/for_export_dpwh_floodcontrol.csv"
+)
+
+display_first_few_rows(
+    "data/well_known_authors_dpwh_floodcontrol.csv"
 )
