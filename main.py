330baa0ac64972c054693cfec6c8aed0025f6a79
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 16:15:30 +0800

    Added comment for the functions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.eda import display_first_few_rows
 
 extract_files_from_zipfile("data/archive.zip")
 
+# Number of rows and columns
 twitter_authors_column_and_row_count = count_rows_and_columns(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
@@ -12,6 +13,7 @@ dpwh_flood_control_tweets_column_and_row_count = count_rows_and_columns(
     "data/for_export_dpwh_floodcontrol.csv"
 )
 
+# Column names and data types
 twitter_authors_columns_and_dtypes = get_column_names_and_dtypes(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
@@ -19,6 +21,7 @@ dpwh_flood_control_tweets_columns_and_dtypes = get_column_names_and_dtypes(
     "data/for_export_dpwh_floodcontrol.csv"
 )
 
+# Preview of first few rows
 twitter_authors_preview = display_first_few_rows(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
