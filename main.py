d9520a496b1eb3c6a2a4d49a9aade193b2d60dac
Christiane Bacani <christianebacani581@gmail.com>
2026-02-22 15:44:45 +0800

    Import the function and not import the module and just call the function by calling also the module name.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from src import eda
+from src.eda import extract_files_from_zipfile
+from src.eda import count_rows_and_columns
+from src.eda import get_column_names_and_dtypes
 
-eda.extract_files_from_zipfile("data/archive.zip")
+extract_files_from_zipfile("data/archive.zip")
 
-twitter_authors_column_and_row_count = eda.count_rows_and_columns(
+twitter_authors_column_and_row_count = count_rows_and_columns(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
-dphwh_flood_control_tweets_column_and_row_count = eda.count_rows_and_columns(
+dphwh_flood_control_tweets_column_and_row_count = count_rows_and_columns(
     "data/for_export_dpwh_floodcontrol.csv"
 )
 
-twitter_authors_columns_and_dtypes = eda.get_column_names_and_dtypes(
+twitter_authors_columns_and_dtypes = get_column_names_and_dtypes(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
