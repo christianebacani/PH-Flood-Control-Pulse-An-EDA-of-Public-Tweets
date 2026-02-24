b978372829e5579e1811f187bbd7a272ebd1c521
christianebacani <crjabacani@bpsu.edu.ph>
2026-02-24 18:18:42 +0800

    Added new comment

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ dpwh_flood_control_tweets_preview = display_first_few_rows(
     "data/for_export_dpwh_floodcontrol.csv"    
 )
 
+# Null count per variable
 twitter_authors_null_count_per_column = get_null_count_per_column(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
