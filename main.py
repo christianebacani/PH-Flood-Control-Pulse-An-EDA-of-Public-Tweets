ab76775268b7b0e37efe66802cfcfbaf6222147f
christianebacani <crjabacani@bpsu.edu.ph>
2026-02-25 18:51:25 +0800

    Update the variable name to the return value of every function

--- main.py
+++ main.py
@@ -7,30 +7,31 @@ from src.eda import get_null_count_per_column
 extract_files_from_zipfile("data/archive.zip")
 
 # Number of rows and columns
-twitter_authors_column_and_row_count = count_rows_and_columns(
+twitter_users_column_and_row_count = count_rows_and_columns(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
-dpwh_flood_control_tweets_column_and_row_count = count_rows_and_columns(
+flood_control_tweets_column_and_row_count = count_rows_and_columns(
     "data/for_export_dpwh_floodcontrol.csv"
 )
 
 # Column names and data types
-twitter_authors_columns_and_dtypes = get_column_names_and_dtypes(
+twitter_users_columns_and_dtypes = get_column_names_and_dtypes(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
-dpwh_flood_control_tweets_columns_and_dtypes = get_column_names_and_dtypes(
+flood_control_tweets_columns_and_dtypes = get_column_names_and_dtypes(
     "data/for_export_dpwh_floodcontrol.csv"
 )
 
 # Preview of first few rows
-twitter_authors_preview = display_first_few_rows(
+twitter_users_preview = display_first_few_rows(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
-dpwh_flood_control_tweets_preview = display_first_few_rows(
+flood_control_tweets_preview = display_first_few_rows(
     "data/for_export_dpwh_floodcontrol.csv"    
 )
 
-twitter_authors_null_count_per_column = get_null_count_per_column(
+# Null count and null count percentage
+twitter_users_null_count_per_column = get_null_count_per_column(
     "data/well_known_authors_dpwh_floodcontrol.csv"
 )
 flood_control_tweets_null_count_per_column = get_null_count_per_column(
