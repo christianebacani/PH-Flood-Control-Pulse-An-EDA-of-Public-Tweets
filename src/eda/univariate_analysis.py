251c7d448c408c1a5952e819cfbad8ee26e30bab
christianebacani <crjabacani@bpsu.edu.ph>
2026-03-01 19:43:09 +0800

    Remove the function
    Remove the function to perform vibe-coding for the univariate analysis.

--- src/eda/univariate_analysis.py
+++ src/eda/univariate_analysis.py
@@ -1,14 +1,1 @@
 import pandas as pd
-
-def summary_statistics(filepath: str):
-    df = pd.read_csv(filepath)
-
-    years = []
-
-    for _, row in df.iterrows():
-        author_createdAt = row["author_createdAt"]
-        year = str(author_createdAt).split("-")[0].strip()
-        years.append(year)
-
-    df["years"] = years
-    print(df)
