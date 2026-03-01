99f3cd86352a59df902c775f2ac5a604a433cd52
christianebacani <crjabacani@bpsu.edu.ph>
2026-03-01 19:00:52 +0800

    Update the module
    Update the content of the function "summary_statistics"

--- src/eda/univariate_analysis.py
+++ src/eda/univariate_analysis.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 def summary_statistics(filepath: str):
     df = pd.read_csv(filepath)
-    columns = df.columns
+
+    years = []
+
+    for _, row in df.iterrows():
+        author_createdAt = row["author_createdAt"]
+        year = str(author_createdAt).split("-")[0].strip()
+        years.append(year)
+
+    df["years"] = years
+    print(df)
